ca3089da912a73fbde64b1e2c77a16b39053e6c3
teror302@wp.pl <teror302@wp.pl>
2018-02-03 01:56:28 +0100

    Log messages

--- MainActivity.java
+++ MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.root.lesson1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String MY_TAG = "Log";
+
+    private static ListView listView;
+
+    private static String[] NAMES = new String[]
+            {"Mietek", "Wladek", "Krzysiu", "Bastek"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listView();
         Log.i(MY_TAG, "onCreate method called!");
     }
 
@@ -30,7 +42,25 @@ public class MainActivity extends AppCompatActivity {
         Log.i(MY_TAG, "onPause method called!");
     }
 
-    public void changeText(View view) {
+    public void listView () {
+        listView = (ListView)findViewById(R.id.listV);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.name_list, NAMES);
+        listView.setAdapter(arrayAdapter);
 
+    }
+
+    public void changeText(View view) {
+        EditText editText = (EditText)findViewById(R.id.editText);
+        TextView textView = (TextView)findViewById(R.id.textView);
+
+        String inputText = editText.getText().toString();
+
+        textView.setText(inputText.toString());
+
+    }
+
+    public void openActivity(View view) {
+        Intent intent = new Intent("com.example.root.lesson1.SecondActivity");
+        startActivity(intent);
     }
 }
